f94d1f86102f1bb4f4f7dee5db83c7933bfdef3d
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-07-24 21:47:26 +0300

    Finish lesson 16, switch web.xml, and apcont.xml to Java classes

---
+++ src/main/java/org/ilie/springcourse/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package org.ilie.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
