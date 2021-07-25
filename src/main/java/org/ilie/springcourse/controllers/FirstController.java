fc5086fd322e210087c27a2fc741924ac2bb739e
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-07-25 09:09:54 +0300

    Finish lesson 20, calculator from parameters

--- src/main/java/org/ilie/springcourse/controllers/FirstController.java
+++ src/main/java/org/ilie/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.ilie.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +16,9 @@ public class FirstController {
     public String helloPage(
             @RequestParam(value = "name", required = false) String name,
             @RequestParam(value = "surname", required = false) String surName
+    ,   Model model
     ) {
-        System.out.println(name);
-        System.out.println(surName);
+        model.addAttribute("message", "Hello "+name+" "+surName+"!");
         return "first/hello";
     }
 
--- src/main/java/org/ilie/springcourse/controllers/SecondController.java
+++ src/main/java/org/ilie/springcourse/controllers/SecondController.java
@@ -2,11 +2,41 @@ package org.ilie.springcourse.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/second")
 public class SecondController {
 
+    @GetMapping("/calculator")
+    public String calc(
+            @RequestParam(value = "a", required = false) int a,
+            @RequestParam(value = "b", required = false) int b,
+            @RequestParam(value = "action", required = false) String action,
+            Model model
+    ) {
+        float result = 0;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / (float) b;
+                break;
+        }
+        model.addAttribute("result", a +" "+ action+" " + b +" = "+result);
+
+        return "second/calc";
+    }
 
     @GetMapping("/exit")
     public String exit() {
