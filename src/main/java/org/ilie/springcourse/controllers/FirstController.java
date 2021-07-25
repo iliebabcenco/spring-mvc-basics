17656165237908292ba94209efb3287df5379858
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-07-25 08:14:09 +0300

    Finish lesson 19 requests with get parameters

--- src/main/java/org/ilie/springcourse/controllers/FirstController.java
+++ src/main/java/org/ilie/springcourse/controllers/FirstController.java
@@ -3,18 +3,27 @@ package org.ilie.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "surname", required = false) String surName
+    ) {
+        System.out.println(name);
+        System.out.println(surName);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage() {
+    public String goodByePage(HttpServletRequest request) {
+        System.out.println(request.getParameter("name"));
         return "first/goodbye";
     }
 }
